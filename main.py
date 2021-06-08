6dff4eb7b5cd2df6ae756b4751581afd8bd1685b
marsex <marsex@protonmail.com>
2021-06-08 14:25:36 -0300

    up

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def start(to):
     try:
         main_loop = asyncio.new_event_loop()
         main_loop.create_task(check_state.start(10))
-        #main_loop.create_task(data_com.start(to))
+        main_loop.create_task(data_com.start(to))
         main_loop.create_task(cam_com.start(to))
-        #main_loop.create_task(ap_cam.start(to))
-        #main_loop.create_task(ap_sv.start(to))
+        main_loop.create_task(ap_cam.start(to))
+        main_loop.create_task(ap_sv.start(to))
         main_loop.run_forever()
     except OSError as e:
         print("async failed"+str(e)) 
